1a8e6e871840cba323261c66ab3a891e095d7497
Jyotendra <85470769+jyotendra02@users.noreply.github.com>
2022-10-18 18:33:45 +0530

    18-10-22

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -18,6 +18,13 @@ void Display(struct array arr)
     }cout<<endl;
 }
 
+void swap(int *x,int *y)
+{
+  int temp = *x;
+  *x=*y;
+  *y = temp;
+}
+
 void Append(struct array *arr,int element )
 {
      if(arr->length < arr->size)
@@ -62,10 +69,113 @@ int LinearSearch(struct array *arr,int key)
   return -1;
 }
 
+int LinearTrans(struct array *arr,int key)
+{
+  int i;
+  for(i=0;i<arr->length;i++)
+  {
+    if(key==arr->a[i])
+    {
+      if(i!=0) //Improvement In lineear Transposition 
+      { 
+       swap(&arr->a[i],&arr->a[i-1]);
+       return i;
+      }
+      return i;
+    }
+  }
+}
+
+int LinearFront(struct array *arr,int key)
+{
+  int i;
+  for(i=0;i<arr->length;i++)
+  {
+    if(key==arr->a[i])
+    {
+      if(i!=0) //Improvement In lineear Transposition 
+      { 
+       swap(&arr->a[i],&arr->a[0]);
+       return i;
+      }
+      return i;
+    }
+  }
+}
+
+int BinarySearch(struct array arr,int key)
+{
+  int low ,high, mid;
+  low =0;
+  high=arr.length-1;
+  
+  while(low <=high)
+  {
+    mid = (low + high)/2;
+    if(key==arr.a[mid])
+     return mid;
+    else if(key<arr.a[mid])
+     return high = mid-1;
+    else 
+     return low = mid +1;
+  }
+  return -1;
+}
+
+int GetIndex(struct array arr,int index)
+{
+  if(index>=0 && index<arr.length)
+   return arr.a[index];
+}
+
+void SetIndex(struct array *arr, int index, int key )
+{ 
+  if(index>=0 && index<arr->length)
+   arr->a[index]=key;
+}
+
+int Max(struct array arr)
+{
+  int max = 0;
+
+  for(int i = 0;i<arr.length;i++)
+  {
+    if (arr.a[i]>max)
+     max = arr.a[i];
+  }
+  return max;
+}
+
+int Min(struct array arr)
+{
+  int min = arr.a[0];
+
+  for(int i = 1;i<arr.length;i++)
+  {
+    if (arr.a[i]<min)
+     min = arr.a[i];
+  }
+  return min;
+}
+
+int Sum(struct array arr)
+{
+  int total=0;
+  for(int i =0;i<arr.length;i++)
+       total += arr.a[i];
+  return total;     
+}
+float Average(struct array arr)
+{
+  return Sum(arr)/(float)arr.length;
+} 
+
 int main()
 {
-    struct array arr = {{2,3,4,5,6},10,5};
+    struct array arr = {{3,4,5,6,2},10,5};
     Display(arr);
-    cout<<LinearSearch(&arr,15);
+    SetIndex(&arr,3,18);
+    Display(arr);
+    cout<<Average(arr);
     return 0;
 }
